81bb6f54f075a8e9f141a46f3410706af3143c08
hscpro <huisc@foxmail.com>
2018-10-19 17:37:50 +0800

    add listtest.py

---
+++ listtest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+data1 = []
+data2 = []
+for indata in sys.argv[1:]:
+    if len(indata)<4:
+        data1.append(indata)
+    if len(indata)>3:
+        data2.append(indata)
+print(data1)
+print(data2)
+
