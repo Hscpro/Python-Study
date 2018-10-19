840c6b315bfd8f10184c55ec460d2f134251bcbf
hscpro <huisc@foxmail.com>
2018-10-19 18:59:02 +0800

    add dicttest.py

---
+++ dicttest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+indata = {}
+for data in sys.argv[1:]:
+  tdata = data.split(':')
+  indata[tdata[0]] = tdata[1]
+for key,value in indata.items():
+  print("ID:{0} Name:{1}".format(key,value))
