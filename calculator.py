2304a3bace4a9a9eb5e3b1311fb9a086dedc194b
Hscpro <huisc@foxmail.com>
2018-10-30 15:26:06 +0800

    楼+挑战二：完善工资计算器

--- calculator.py
+++ calculator.py
@@ -1,29 +1,65 @@
 #!/usr/bin/env python3
-
 import sys
 
-gz = sys.argv[1]
 
-try:
-    gzint = int(gz)
-    qisui = gzint - 3500
-    sui = 0
-    if qisui < 1:
-        sui = 0
-    elif qisui < 1501:
-        sui = qisui * 0.03 - 0
-    elif qisui < 4501:
-        sui = qisui * 0.1 - 105
-    elif qisui < 9001:
-        sui = qisui * 0.2 - 555
-    elif qisui < 35001:
-        sui = qisui * 0.25 - 1005
-    elif qisui < 55001:
-        sui = qisui * 0.3 - 2755
-    elif qisui < 80001:
-        sui = qisui * 0.35 - 5505
-    elif qisui > 80000:
-        sui = qisui * 0.45 - 13505
-    print("{:.2f}".format(sui, ".2f"))
-except:
-    print("Parameter Error")
+def Rins():
+    ID_Dat = []
+    NA_Dat = []
+    try:
+        for ins in sys.argv[1:]:
+            tmp_in = ins.split(':')
+            ID_Dat.append(tmp_in[0])
+            NA_Dat.append(int(tmp_in[1]))
+        return ID_Dat, NA_Dat
+    except:
+        print("Parameter Error")
+
+
+def Count(dat):
+    try:
+        bind = len(dat)
+        bins = 0
+        while bins < bind:
+            if dat[bins] <= 0:
+                return dat[bins]
+            else:
+                sefat = dat[bins] * 0.165
+                ifdat = dat[bins] - (3500 + sefat)
+                if ifdat < 0:
+                    dat[bins] = dat[bins]
+                elif ifdat <= 1500:
+                    dat[bins] -= ifdat * 0.03 - 0
+                elif ifdat <= 4500:
+                    dat[bins] -= ifdat * 0.1 - 105
+                elif ifdat <= 9000:
+                    dat[bins] -= ifdat * 0.2 - 555
+                elif ifdat <= 35000:
+                    dat[bins] -= ifdat * 0.25 - 1005
+                elif ifdat <= 55000:
+                    dat[bins] -= ifdat * 0.3 - 2755
+                elif ifdat <= 80000:
+                    dat[bins] -= ifdat * 0.35 - 5505
+                else:
+                    dat[bins] -= ifdat * 0.45 - 13505
+                dat[bins] -= sefat
+            bins += 1
+        return dat
+    except:
+        print("Parameter Error")
+
+
+def Display(iddat, nadat):
+    bind = len(iddat)
+    bins = 0
+    while bins < bind:
+        print("{0}:{1:.2f}".format(iddat[bins], nadat[bins]))
+        bins += 1
+
+
+if __name__ == '__main__':
+    try:
+        Indata = Rins()
+        Codata = Count(Indata[1])
+        Display(Indata[0], Codata)
+    except:
+        print("Parameter Error")
